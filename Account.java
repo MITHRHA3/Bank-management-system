aab6f83c4f190834303b021c54a5c558a49b58b0
MITHRHA3 <yuvarajamithrha@gmail.com>
2025-09-21 16:26:00 +0530

    Add basic Account class with constructor and fields

---
+++ Account.java
@@ -0,0 +1,49 @@
+public class Account {
+    private String accountNumber;
+    private String holderName;
+    private double balance;
+    
+    public Account(String accountNumber, String holderName, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = initialDeposit;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public String getHolderName() {
+        return holderName;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean transfer(Account targetAccount, double amount) {
+        if (amount > 0 && amount <= balance) {
+            if (this.withdraw(amount)) {
+                targetAccount.deposit(amount);
+                return true;
+            }
+        }
+        return false;
+    }
+}
