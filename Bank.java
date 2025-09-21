d4287f38b8ddafb66179fc1f690a79bfd78841f8
MITHRHA3 <yuvarajamithrha@gmail.com>
2025-09-21 16:26:16 +0530

    Add Bank class with account management methods

---
+++ Bank.java
@@ -0,0 +1,66 @@
+public class Bank {
+    private Account[] accounts;
+    private int count;
+    
+    public Bank() {
+        accounts = new Account[100];
+        count = 0;
+    }
+    
+    public void createAccount(String accountNumber, String holderName, double initialDeposit) {
+        if (count < accounts.length) {
+            accounts[count] = new Account(accountNumber, holderName, initialDeposit);
+            count++;
+        }
+    }
+    
+    public Account findAccount(String accountNumber) {
+        for (int i = 0; i < count; i++) {
+            if (accounts[i].getAccountNumber().equals(accountNumber)) {
+                return accounts[i];
+            }
+        }
+        return null;
+    }
+    
+    public boolean deposit(String accountNumber, double amount) {
+        Account account = findAccount(accountNumber);
+        if (account != null) {
+            return account.deposit(amount);
+        }
+        return false;
+    }
+    
+    public boolean withdraw(String accountNumber, double amount) {
+        Account account = findAccount(accountNumber);
+        if (account != null) {
+            return account.withdraw(amount);
+        }
+        return false;
+    }
+    
+    public boolean transfer(String fromAccount, String toAccount, double amount) {
+        Account from = findAccount(fromAccount);
+        Account to = findAccount(toAccount);
+        if (from != null && to != null) {
+            return from.transfer(to, amount);
+        }
+        return false;
+    }
+    
+    public void listAccounts() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(accounts[i].getAccountNumber() + " - " + 
+                             accounts[i].getHolderName() + " - $" + 
+                             accounts[i].getBalance());
+        }
+    }
+    
+    public double getTotalBalance() {
+        double total = 0;
+        for (int i = 0; i < count; i++) {
+            total += accounts[i].getBalance();
+        }
+        return total;
+    }
+}
